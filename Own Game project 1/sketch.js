7c1ba0aed595b76f3fa94c47f6d145f775f15cf4
Ruald12 <70239329+Ruald12@users.noreply.github.com>
2021-01-15 14:22:59 -0700

    Add files via upload

---
+++ Own Game project 1/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+
+/* Game Story:
+Many space companys and orgnizations set out to claim resources 
+through out the solor system. Your company joins the race, your job is to collect 
+resources on the planet so you can reach the next planet and beat the competeters  
+
+   Pc and NPC 
+the character collecting the resources is the Pc, the other companys are the NPC's.*/
